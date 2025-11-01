bd0d2c4fdbf3a1803c8caa771839fa153d348e67
Tushar Kasaudhan <tusharkasaudhan6@gmail.com>
2025-11-01 13:45:36 +0000

    apiKey: "AIzaSyCjbcA1oZk3ExNVMMjHiyBn9MJyF6JGubM",   authDomain: "studio

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-6737588015-fde4d",
-  "appId": "1:637302429275:web:e7c488958784a9f2aad17e",
   "apiKey": "AIzaSyCjbcA1oZk3ExNVMMjHiyBn9MJyF6JGubM",
   "authDomain": "studio-6737588015-fde4d.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "637302429275"
+  "projectId": "studio-6737588015-fde4d",
+  "storageBucket": "studio-6737588015-fde4d.appspot.com",
+  "messagingSenderId": "637302429275",
+  "appId": "1:637302429275:web:e7c488958784a9f2aad17e"
 };
