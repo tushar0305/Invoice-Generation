f07bfdda0455bc2193271cbafc82a53aeb460903
Tushar Kasaudhan <tusharkasaudhan6@gmail.com>
2025-11-03 18:33:25 +0000

    where have you implemented firebase config const firebaseConfig = {   a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "apiKey": "AIzaSyCjbcA1oZk3ExNVMMjHiyBn9MJyF6JGubM",
   "authDomain": "studio-6737588015-fde4d.firebaseapp.com",
   "projectId": "studio-6737588015-fde4d",
-  "storageBucket": "studio-6737588015-fde4d.appspot.com",
+  "storageBucket": "studio-6737588015-fde4d.firebasestorage.app",
   "messagingSenderId": "637302429275",
   "appId": "1:637302429275:web:e7c488958784a9f2aad17e"
 };
